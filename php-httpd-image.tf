f88592ebf26b5a5149fae15051cee6dab66a1fab
Kescefko <78681186+Kescefko@users.noreply.github.com>
2025-02-11 21:00:23 +0100

    Create php-httpd-image.tf

---
+++ php-httpd-image.tf
@@ -0,0 +1,7 @@
+resource "docker_image" "php-httpd-image" {
+  name         = "php-httpd:challenge"
+  build {
+    path    = "lamp_stack/php_httpd"
+    label      = { "challenge" = "second" }
+  }
+}
